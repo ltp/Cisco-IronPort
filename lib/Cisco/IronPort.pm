6805dd3f4bd02c438120c9ec7f0d1034078cdcc9
Luke Poskitt <luke.poskitt@gmail.com>
2013-05-27 16:49:55 +1000

    Added several new methods

--- lib/Cisco/IronPort.pm
+++ lib/Cisco/IronPort.pm
@@ -108,7 +108,7 @@ our %M_MAP	= (
 							report_query_id	=> 'mga_content_filters_top_outgoing_cf_matches',
 							sortby		=> 'begin_timestamp'
 							},
-		top_outgoing_content_filter_matches	=> {
+		top_incoming_content_filter_matches	=> {
 							report_def_id	=> 'mga_content_filters',
 							report_query_id	=> 'mga_content_filters_top_incoming_cf_matches',
 							sortby		=> 'begin_timestamp'
@@ -118,12 +118,25 @@ our %M_MAP	= (
 							report_query_id	=> 'mga_content_filters_incoming_cf_matches',
 							sortby		=> 'begin_timestamp'
 							},
-		incoming_content_filter_matches		=> {
+		outgoing_content_filter_matches		=> {
 							report_def_id	=> 'mga_content_filters',
 							report_query_id	=> 'mga_content_filters_outgoing_cf_matches',
 							sortby		=> 'begin_timestamp'
 							},
-						
+		content_filter_detail			=> {
+							filter_type	=> 'outgoing',
+							param		=> 'filter_name',
+							report		=> 'content_filter_detail',
+							report_def_id	=> 'mga_content_filter_detail',
+							report_query_id	=> 'mga_content_filter_detail_content_filter_by_user',
+							sortby		=> 'begin_timestamp'
+							},
+		outgoing_delivery_status		=> {
+							report_def_id	=> 'mga_outgoing_delivery_status',
+							report_query_id	=> 'mga_outgoing_delivery_status_status_table',
+							sortby		=> 'destination_domain'
+							}
+
 		# profile_type=all
 		# format=csv
 		# report_query_id=mga_system_capacity_average_messages_workqueue
@@ -149,22 +162,33 @@ sub new {
 
 	foreach my $m (keys %M_MAP) {
 		*{ __PACKAGE__ . '::__' . $m } = sub {
-			my ($self,%args) = @_;
-			return $self->__request("report?format=csv&date_range=$args{date_range}&" .
+			my ($self,$args) = @_;
+
+			return ( exists $M_MAP{$m}{param} 
+
+			? 	$self->__request("$M_MAP{$m}{report}?format=csv&date_range=$args->{date_range}&".
+						"report_query_id=$M_MAP{$m}{report_query_id}&" .
+						"report_def_id=$M_MAP{$m}{report_def_id}&" .
+						"filter_type=outgoing&filter_name=$args->{filter_name}")
+
+			:	$self->__request("report?format=csv&date_range=$args->{date_range}&" .
 						"report_query_id=$M_MAP{$m}{report_query_id}&" .
 						"report_def_id=$M_MAP{$m}{report_def_id}&profile_type=all")
+			)
 		};
 
 		foreach my $range (@RANGES) {
 			my $p = ($m =~ /summary/ ? '__parse_summary' : '__parse_statistics');
 			*{ __PACKAGE__ . '::' . $m . '_' . $range } = sub {
-				my $self = shift;
+				my ($self,$args) = @_;
+				$args->{date_range} = $range;
 				my $f = '__'.$m;
-				return $p->($self->$f(date_range => $range), $M_MAP{$m}{sortby})
+				return $p->($self->$f($args), $M_MAP{$m}{sortby})
 			};
 
 			*{ __PACKAGE__ . '::' . $m . '_' . $range . '_raw' } = sub {
-				my $self = shift;
+				my ($self,$args) = @_;
+				$args->{date_range} = $range;
 				my $f = '__'.$m;
 				return $self->$f(date_range => $range)
 			};
@@ -237,6 +261,10 @@ sub __parse_summary {
 
 	return %res
 }
+
+1;
+
+__END__
 
 =head1 NAME
 
@@ -730,6 +758,46 @@ period as retrieved directly from the reporting API.
 
 This method may be useful if you wish to process the raw data retrieved from the API yourself.
 
+=head2 outgoing_content_filter_matches_current_hour
+
+This method returns a nested hash containing statistics for outgoing content filter matches for the 
+previous hourly period - the hash has the following structure:
+
+	measurement_period_1_begin_timestamp => {
+	  begin_timestamp	=> a timestamp marking the beginning of the measurement period in seconds since epoch,
+	  end_timestamp		=> a timestamp marking the ending of the measurement period in seconds since epoch,
+	  begin_date		=> a human-readable timestamp marking the beginning of the measurement period (YYYY-MM-DD HH:MM:SS TZ),
+	  end_date		=> a human-readable timestamp marking the ending of the measurement period (YYYY-MM-DD HH:MM:SS TZ),
+	  content_filter	=> the name of the content filter,
+	  messages		=> the number of outgoing messages matched by the content filter in the previous hour period,
+	},
+	measurement_period_2_begin_timestamp => {
+	  ...
+	},
+	...
+	measurement_period_n_begin_timestamp => {
+	  ...
+	}
+
+=head2 outgoing_content_filter_matches_current_hour_raw
+
+Returns a scalar containing statistics for outgoing content filter matches for the previous hourly
+period as retrieved directly from the reporting API.
+
+This method may be useful if you wish to process the raw data retrieved from the API yourself.
+
+=head2 outgoing_content_filter_matches_current_day
+
+Returns a nested hash containing statistics for outgoing content filter matches for the previous daily period 
+- the hash has the same structure as detailed in the B<outgoing_content_filter_matches_current_hour> above.
+
+=head2 outgoing_content_filter_matches_current_day_raw
+
+Returns a scalar containing statistics for the outgoing content filter matches for the previous daily
+period as retrieved directly from the reporting API.
+
+This method may be useful if you wish to process the raw data retrieved from the API yourself.
+
 =head2 maximum_messages_in_workqueue_current_hour
 
 This method returns a nested hash containing statistics for the maximum number of messages in the workqueue for
@@ -922,7 +990,47 @@ Returns a nested hash containing statistics for the top outgoing content filter 
 
 =head2 top_outgoing_content_filter_matches_current_day_raw
 
-Returns a scalar containing statistics for the average time a message spent in the workqueue for the previous daily
+Returns a nested hash containing statistics for the top outgoing content filter matches for the previous daily 
+period as retrieved directly from the reporting API.
+
+This method may be useful if you wish to process the raw data retrieved from the API yourself.
+
+=head2 top_incoming_content_filter_matches_current_hour
+
+This method returns a nested hash containing statistics for the top incoming content filter matches for the 
+previous hourly period - the hash has the following structure:
+
+	measurement_period_1_begin_timestamp => {
+	  begin_timestamp	=> a timestamp marking the beginning of the measurement period in seconds since epoch,
+	  end_timestamp		=> a timestamp marking the ending of the measurement period in seconds since epoch,
+	  begin_date		=> a human-readable timestamp marking the beginning of the measurement period (YYYY-MM-DD HH:MM:SS TZ),
+	  end_date		=> a human-readable timestamp marking the ending of the measurement period (YYYY-MM-DD HH:MM:SS TZ),
+	  content_filter	=> the name of the content filter,
+	  messages		=> the number of incoming messages matched by the content filter in the previous hour period
+	},
+	measurement_period_2_begin_timestamp => {
+	  ...
+	},
+	...
+	measurement_period_n_begin_timestamp => {
+	  ...
+	}
+
+=head2 top_incoming_content_filter_matches_current_hour_raw
+
+Returns a scalar containing statistics for the top incoming content content filter matches for the previous hourly
+period as retrieved directly from the reporting API.
+
+This method may be useful if you wish to process the raw data retrieved from the API yourself.
+
+=head2 top_incoming_content_filter_matches_current_day
+
+Returns a nested hash containing statistics for the top incoming content filter matches for the previous daily period 
+- the hash has the same structure as detailed in the B<top_incoming_content_filter_matches_current_hour> above.
+
+=head2 top_incoming_content_filter_matches_current_day_raw
+
+Returns a scalar containing statistics for the top incoming content content filter matches for the previous daily
 period as retrieved directly from the reporting API.
 
 This method may be useful if you wish to process the raw data retrieved from the API yourself.
@@ -1211,6 +1319,30 @@ period as retrieved directly from the reporting API.
 
 This method may be useful if you wish to process the raw data retrieved from the API yourself.
 
+=head2 content_filter_detail_current_hour ( { filter_name => $filter_name } )
+
+Given an anonymous hash containing the key 'filter_name' where the value is a valid content filter
+name defined on the target device, this method returns a nested hash containing details of the 
+content filter statistics for the previous hourly period - the hash has the following structure:
+
+	measurement_period_1_begin_timestamp => {
+	  begin_timestamp	=> a timestamp marking the beginning of the measurement period in seconds since epoch,
+	  end_timestamp		=> a timestamp marking the ending of the measurement period in seconds since epoch,
+	  begin_date		=> a human-readable timestamp marking the beginning of the measurement period (YYYY-MM-DD HH:MM:SS TZ),
+	  end_date		=> a human-readable timestamp marking the ending of the measurement period (YYYY-MM-DD HH:MM:SS TZ),
+	  key			=> the user-specified name of the content filter that was matched,
+	  internal_user		=> the email address of the internal user sending the email matching the content filter,
+	  messages		=> the total number of messages matching this content_filter for the measurement period
+	},
+	measurement_period_1_begin_timestamp => {
+	  
+=head2 content_filter_detail_current_hour ( { filter_name => $filter_name } )
+
+Given an anonymous hash containing the key 'filter_name' where the value is a valid content filter
+name defined on the target device, this method returns a nested hash containing details of the 
+content filter statistics for the previous hourly period - the hash has the following structure:
+
+
 =cut
 
 
@@ -1272,4 +1404,3 @@ See http://dev.perl.org/licenses/ for more information.
 
 =cut
 
-1;
